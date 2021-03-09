dfd4f966f1909279841592972263054fcbcebc6d
FirmanMulyawan <firmanmulyawan491@gmail.com>
2021-03-09 16:22:52 +0700

    end post

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../Styles.css'
 
 const Login = () => {
 	return (
 		<div>
-			<p>Login</p>
+			<h2>Login</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue
+				tortor, laoreet et nulla et, venenatis tempor elit. Nam quis nunc
+				sapien. Integer gravida nisi eu erat semper eleifend. Etiam ullamcorper
+				metus non enim congue, et ornare nisi auctor. Nullam at ullamcorper
+				elit. Integer mattis vulputate pellentesque.
+			</p>
 		</div>
 	)
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import '../Styles.css'
 
 const Register = () => {
 	return (
 		<div>
-			<p>Register</p>
-		</div>
+        <h2>Register</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Morbi augue tortor, laoreet et nulla et, venenatis tempor elit. 
+Nam quis nunc sapien. Integer gravida nisi eu erat semper eleifend. 
+Etiam ullamcorper metus non enim congue, et ornare nisi auctor. 
+Nullam at ullamcorper elit. Integer mattis vulputate pellentesque.</p>
+        
+      </div>
 	)
 }
 
